4eb8b935ca13758cf3e414e44f4307a886204d97
YousefSaeed668 <137455394+YousefSaeed668@users.noreply.github.com>
2023-09-10 12:06:05 +0300

    main.js

--- main.js
+++ main.js
@@ -36,7 +36,22 @@ var swiper = new Swiper(".home", {
     clickable: true,
   },
 });
-
+var swiper = new Swiper(".control-images", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 let accordionItem = document.querySelectorAll(".accordion-item");
 
 accordionItem.forEach((item) => {
